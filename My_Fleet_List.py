32677f2ea4af716d6b8994187823f669938f70a7
navaliira <navid.ghavami@icvt.uni-stuttgart.de>
2019-06-22 17:36:22 +0200

    Create My_Fleet_List.py

---
+++ My_Fleet_List.py
@@ -0,0 +1,50 @@
+import json
+import requests
+
+"""
+Input parameters; e.g. API Token, groupId, baseUrl, etc.
+"""
+access_token = '8lRABQGYyah8lpcvB2r1uGk4FYkkUN'
+groupId = 33959
+baseUrl = 'https://api.samsara.com/v1'
+
+"""
+Response Codes Descriptions
+"""
+def responseCodes(response):
+	"""
+	This function performs error handling for the API calls.
+	"""
+	if response.status_code >= 200 and response.status_code < 299:
+	#Do nothing, API call was successful
+		pass
+	elif response.status_code == 400:
+		print(response.text)
+		raise ValueError('Bad Request: Please make sure the request follows the format specified in the documentation.')
+	elif response.status_code == 401:
+		print(response.text)
+		raise ValueError('Invalid Token: Could not authenticate successfully')
+	elif response.status_code == 404:
+		print(response.text)
+		raise ValueError('Page not found: API Endpoint is invalid')
+	else:
+		print(response.text)
+		raise ValueError('Request was not successful')
+
+
+"""
+Find Sensor(s) ID
+"""
+def getSensorsList(access_token,groupId):
+	sensorsListUrl = '/sensors/list'
+	parameters = {"access_token":access_token}
+
+	requestBody = {"groupId":groupId}
+
+	response = requests.get(baseUrl+sensorsListUrl,params=parameters,json=requestBody)
+
+	responseCodes(response)
+	return response.json()['sensors']
+
+#Sample run
+print(getSensorsList(access_token,groupId))
