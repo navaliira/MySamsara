d948fac3d04c025fb2d2c3f4f7a0e07313f6f5be
navaliira <navaliira@yahoo.com>
2019-06-22 17:53:48 +0200

    Create My_Temperature.py
    Displaying the most current recorded temperature

---
+++ My_Temperature.py
@@ -0,0 +1,56 @@
+import json
+import requests
+
+"""
+Input parameters; e.g. API Token, groupId, baseUrl, etc.
+"""
+access_token = '8lRABQGYyah8lpcvB2r1uGk4FYkkUN'
+groupId = 33959
+baseUrl = 'https://api.samsara.com/v1'
+sensorId_Environment = 212014918414714
+
+
+"""
+Response Codes Descriptions
+"""
+def responseCodes(response):
+	"""
+	This function performs error handling for the API calls.
+	"""
+	if response.status_code >= 200 and response.status_code < 299:
+	#Do nothing, API call was successful
+		pass
+	elif response.status_code == 400:
+		print(response.text)
+		raise ValueError('Bad Request: Please make sure the request follows the format specified in the documentation.')
+	elif response.status_code == 401:
+		print(response.text)
+		raise ValueError('Invalid Token: Could not authenticate successfully')
+	elif response.status_code == 404:
+		print(response.text)
+		raise ValueError('Page not found: API Endpoint is invalid')
+	else:
+		print(response.text)
+		raise ValueError('Request was not successful')
+
+        
+def getSensorsTemperature(access_token,groupId):
+	sensorsTemperatureUrl = '/sensors/temperature'
+
+	parameters = {"access_token":access_token}
+
+	requestBody= {
+			"groupId": groupId,
+			"sensors": [sensorId_Environment]
+			}
+
+	response = requests.get(baseUrl+sensorsTemperatureUrl,params=parameters,json=requestBody)
+
+	responseCodes(response)
+	return response.json()['sensors']
+
+#Saving Result only as a dictionary
+Result = getSensorsTemperature(access_token,groupId)[0]
+
+#Display of Temperature
+print("Current Temperature = ",Result['ambientTemperature']/1000, " °C")
